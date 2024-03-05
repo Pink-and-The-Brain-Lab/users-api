5ddda003b90d1829a9d100a7517ef7ac97608b37
André Lagacione <lagacioneandre@gmail.com>
2024-03-04 19:26:26 -0500

    fix error messaage

--- src/utils/validate-email.ts
+++ src/utils/validate-email.ts
@@ -2,5 +2,5 @@ import { AppError, ValidateEmail } from "millez-lib-api";
 
 export const validateEmail = (email: string) => {
     const validateEmail = new ValidateEmail().validate(email);
-    if (!validateEmail) throw new AppError('INVALID_EMAIL');
+    if (!validateEmail) throw new AppError('API_ERRORS.INVALID_EMAIL');
 };
