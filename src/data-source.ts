8144e07350aabba4e0d17d35c70324e4038baff6
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-02-12 07:50:42 -0300

    change to use env configs

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,14 +3,21 @@ import { DataSource } from "typeorm";
 import { CreateUsers1690416692582 } from "./database/migrations/1690416692582-CreateUsers";
 import User from "./models/user.model";
 import 'dotenv/config';
+import { config } from "dotenv";
+
+if (process.env.NODE_ENV === 'test') {
+    config({ path: './.env.test' });
+} else {
+    config();
+}
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT as string, 10),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: "millez-users",
+    database: "postgres",
     synchronize: false,
     logging: false,
     entities: [
