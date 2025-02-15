fea77a6c2032996520fa837131e74cc536057d7c
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-02-15 11:04:13 -0300

    change file name

---
+++ src/routes/reset-password.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { IResetPassword } from "./interfaces/reset-password.interface";
+import { RabbitMqQueues } from "../enums/rabbitmq-queues.enum";
+import { AppError, RabbitMqManageConnection, RabbitMqMessagesProducerService, ValidateEmail } from "millez-lib-api";
+import { IValidationTokenData } from "../services/interfaces/validation-token-data.interface";
+import { RABBITMQ_HOST_URL } from "../constants/rabbitmq-host-url";
+const resetPasswordRouter = Router();
+
+resetPasswordRouter.post('/', async (request: Request<IResetPassword>, response: Response, next: NextFunction) => {
+    try {
+        const { email } = request.body;
+        const validateEmail = new ValidateEmail().validate(email);
+        if (!validateEmail) throw new AppError('API_ERRORS.INVALID_EMAIL');
+        const connection = new RabbitMqManageConnection(RABBITMQ_HOST_URL);
+        const rabbitMqService = new RabbitMqMessagesProducerService(connection);
+        const tokenApiResponse = await rabbitMqService.sendDataToAPI<string, IValidationTokenData>(email, RabbitMqQueues.CREATE_TOKEN, RabbitMqQueues.USER_RESPONSE_QUEUE);
+        if (tokenApiResponse.statusCode)
+            throw new AppError(tokenApiResponse.message || 'Internal server error.', tokenApiResponse.statusCode);
+        return response.json(tokenApiResponse);
+    } catch (error) {
+        next(error)
+    }
+});
+
+export default resetPasswordRouter;
