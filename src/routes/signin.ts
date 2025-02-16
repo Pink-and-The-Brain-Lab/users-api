c9a56fe194ec12e825bd94fd02dcc782991aab2c
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-02-16 15:03:48 -0300

    rename files

---
+++ src/routes/signin.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response, Router } from "express";
+import SigninService from "../services/SigninService";
+import { ISignin } from "./interfaces/signin.interface";
+const signinRouter = Router();
+
+signinRouter.post('/', async (request: Request<ISignin>, response: Response, next: NextFunction) => {
+    try {
+        const { email, password, keepLoggedIn } = request.body;
+        const signinService = new SigninService();
+        const login = await signinService.execute({ email, password, keepLoggedIn });
+        return response.json(login);
+    } catch (error) {
+        next(error)
+    }
+});
+
+export default signinRouter;
