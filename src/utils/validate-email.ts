68f1185b229280ad04dfb2024d5074928d2f5752
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-03-11 07:52:31 -0300

    remove class

--- src/utils/validate-email.ts
+++
@@ -1,6 +0,0 @@
-import { AppError, ValidateEmail } from "millez-lib-api";
-
-export const validateEmail = (email: string) => {
-    const validateEmail = new ValidateEmail().validate(email);
-    if (!validateEmail) throw new AppError('API_ERRORS.INVALID_EMAIL');
-};
