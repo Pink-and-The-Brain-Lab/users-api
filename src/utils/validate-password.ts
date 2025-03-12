cc7edb132b5aa6749466d0df0012438d55566078
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-03-12 08:30:28 -0300

    remove class

--- src/utils/validate-password.ts
+++
@@ -1,6 +0,0 @@
-import { AppError, ValidatePassword } from "millez-lib-api";
-
-export const validatePassword = (password: string, confirmPassword: string) => {
-    const validatePassword = new ValidatePassword(8).validate(password, confirmPassword);
-    if (validatePassword.length) throw new AppError(validatePassword);
-};
